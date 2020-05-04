8280fc059c4a8030f1e684d4070738e4bc8d3809
Anton <aglanton31@gmail.com>
2020-05-04 10:58:07 +0300

    ALLOWED_HOSTS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'quiet-plateau-76980',
+]
 
 
 # Application definition
